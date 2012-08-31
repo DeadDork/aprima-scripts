37d893bc1ef5764ed71c790719b3efd8611bab63
Nimrod Omer <nimrodomer@gmail.com>
2012-08-31 10:13:15 -0700

    Added password prompt (less secure, but oh so convenient).

--- visit_notes_exporter/export.sh
+++ visit_notes_exporter/export.sh
@@ -18,8 +18,9 @@
 # Aprima's patient notes bulk export function is broken. This script let's a user with access to the DBMS to quickly export patient visit notes.
 
 # Prompt and read user input for a number of variables.
-read -p 'Sqsh user name (e.g. "domain\\nimrod")? ' user
 read -p 'Server name to connect to with sqsh (e.g. tnh_mss)? ' server
+read -p 'Sqsh user name (e.g. "domain\\nimrod")? ' user
+read -p 'Sqsh password (e.g. "yeah right, buddy")? ' password
 
 read -p 'Patient first name? ' px_FirstName
 read -p 'Patient last name? ' px_LastName
@@ -32,13 +33,13 @@ read -p 'Doctor last name? ' rx_LastName
 
 # Determine the patient whose doctor visit notes are desires for export, as well as the doctor who treated the patient.
 echo "SQSH:"
-sqsh -S "${server}" -U "${user}" -C "select FirstName, LastName, BirthDate, cast(PersonUid as varchar(max)) as PersonUid from prm.dbo.person where (FirstName = '${px_FirstName}' and LastName = '${px_LastName}') or (FirstName = '${rx_FirstName}' and LastName = '${rx_LastName}')" -m pretty
+sqsh -S "${server}" -U "${user}" -P "${password}" -C "select FirstName, LastName, BirthDate, cast(PersonUid as varchar(max)) as PersonUid from prm.dbo.person where (FirstName = '${px_FirstName}' and LastName = '${px_LastName}') or (FirstName = '${rx_FirstName}' and LastName = '${rx_LastName}')" -m pretty
 read -p 'Copy and paste the PersonUid of the patient whose visit notes you want to export. ' PatientUid
 read -p 'Copy and paste the PersonUid of the doctor of the patient whose visit notes you want to export. ' DoctorUid
 
 # A csv of each visit note's unique identifier & the date of the note.
 echo "SQSH:"
-sqsh -S "${server}" -U "${user}" -C "select cast(VisitUid as varchar(max)), cast(year(VisitDate) as varchar(max)) + '-' + cast(month(VisitDate) as varchar(max)) + '-' + cast(day(VisitDate) as varchar(max)) + '_' + cast(cast(VisitDate as time) as varchar(max)) from prm.dbo.visit where PatientUid = '${PatientUid}' and ProviderUid = '${DoctorUid}'" -L bcp_colsep='|' -L bcp_rowsep='' -m bcp > /tmp/aprima_VisitNotes_export.csv
+sqsh -S "${server}" -U "${user}" -P "${password}" -C "select cast(VisitUid as varchar(max)), cast(year(VisitDate) as varchar(max)) + '-' + cast(month(VisitDate) as varchar(max)) + '-' + cast(day(VisitDate) as varchar(max)) + '_' + cast(cast(VisitDate as time) as varchar(max)) from prm.dbo.visit where PatientUid = '${PatientUid}' and ProviderUid = '${DoctorUid}'" -L bcp_colsep='|' -L bcp_rowsep='' -m bcp > /tmp/aprima_VisitNotes_export.csv
 
 # Generate sqsh scripts from the above csv that, when executed, will produce the visit notes.
 awk 'BEGIN {FS="|"} {print "select\n\t\tCCComment\n\tfrom\n\t\tprm.dbo.VisitComment\n\twhere\n\t\tVisitUid = '\''" $1 "'\''\n\\go -m bcp > '"${output_folder}/${px_FirstName}_${px_LastName}_"'" $2 ".rtf"}' < /tmp/aprima_VisitNotes_export.csv > /tmp/aprima_VisitNotes_export.sqsh
@@ -48,6 +49,6 @@ awk 'BEGIN {FS="|"} {print "select\n\t\tCCComment\n\tfrom\n\t\tprm.dbo.VisitComm
 
 # Execute the sqsh script that will generate the visit notes rtf files.
 echo "SQSH:"
-sqsh -S "${server}" -U "${user}" -L bcp_colsep='' -L bcp_rowsep='' -i /tmp/aprima_VisitNotes_export.sqsh
+sqsh -S "${server}" -U "${user}" -P "${password}" -L bcp_colsep='' -L bcp_rowsep='' -i /tmp/aprima_VisitNotes_export.sqsh
 
 exit 0
