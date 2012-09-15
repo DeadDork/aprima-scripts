4b9c2482b94f1d78dd36596157920bd17a5f72ca
DeadDork <nimrodomer@gmail.com>
2012-09-14 21:34:28 -0700

    Cleaned up the code, made it more readable.

--- inflator/inflator.pl
+++ inflator/inflator.pl
@@ -5,22 +5,27 @@ use strict;
 use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError);
 
 # The directory where the compressed files (might) be.
-print "Enter the patient directory (N.B. don't specify to the further layers of <compressed> or <decompressed>!!!), e.g. </home/work_two/samba/share/TNH/projects/data_transfer/single_patient_file_export/patients/LASTNAME_FIRSTNAME/>: ";
-chomp(my $export_dir = <>);
+print "Enter the patient directory (N.B. don't specify to the further layers of <compressed> or <decompressed>!!! Just to the directory above them, e.g. </home/work_two/samba/share/TNH/projects/data_transfer/single_patient_file_export/patients/LASTNAME_FIRSTNAME/>: ";
+chomp(my $top_level_dir = <>);
 
 # The compressed file directory.
-my $input = $export_dir . "compressed/";
+my $compressed_dir = $top_level_dir . "compressed/";
 
 # The uncompressed file directory.
-my $output = $export_dir . "uncompressed/";
+my $uncompressed_dir = $top_level_dir . "uncompressed/";
 
 # Opens the compressed directory & decompresses any files it finds in it into the the <uncompressed> directory.
-opendir(my $dir_handle, $input) or die "Couldn't open $input: $!\n";
+opendir(my $dir_handle, $compressed_dir) or die "Couldn't open $input: $!\n";
 while(readdir $dir_handle) {
-	my $input = $input . $_;
-	my $output = $output . $_;
+
+	# The compressed file.
+	my $compressed_file = $compressed_dir . $_;
+
+	# The uncompressed file.
+	my $uncompressed_file = $uncompressed_dir . $_;
+
 	unless ($_ =~ /^\.+$/) {
-		anyuncompress $input => $output or die "anyuncompress failed: $AnyUncompressError\n";
+		anyuncompress $compressed_file => $uncompressed_file or die "anyuncompress failed: $AnyUncompressError\n";
 	}
 }
 closedir $dir_handle;
